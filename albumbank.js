3055c4f1e5642b657ec9e6c08d231a08ca645a56
Achmad Rivaldi <33930946+mrtampan@users.noreply.github.com>
2022-05-15 16:38:04 +0700

    Update albumbank.js

--- albumbank.js
+++ albumbank.js
@@ -1,3 +1,4 @@
 var albumBank = [
-        { name: 'Caem Bank', url: 'https://mrtampan.github.io/caem_bank/'}
+        { name: 'Caem Bank', url: 'https://mrtampan.github.io/caem_bank/'},
+        { name: 'Kaori Song', url: 'https://mrtampan.github.io/example_kaorimusic/'},
     ]
